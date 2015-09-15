76f77732c8e25bd3c2768d082153d7ae9c4b70fe
Pawel Wocjan <wocjan@eecs.ucf.edu>
2015-09-15 19:03:59 -0400

    some changes

--- lectures/haskell-functional/InsertWhen.hs
+++ lectures/haskell-functional/InsertWhen.hs
@@ -2,6 +2,7 @@ module InsertWhen where
 
 insertWhen _ _ [] = []
 
-insertWhen pred element (x:xs) = if pred x 
-                                   then element:x:(insertWhen pred element xs)
-                                   else x:(insertWhen pred element xs)
+insertWhen pred element (x:xs) = 
+  if pred x 
+    then element:x:(insertWhen pred element xs)
+    else x:(insertWhen pred element xs)
