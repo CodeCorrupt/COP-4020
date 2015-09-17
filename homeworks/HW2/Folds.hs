d74779a0d47f326c345b17e886fd0b23817792a2
Pawel Wocjan <wocjan@eecs.ucf.edu>
2015-09-17 13:48:18 -0400

    HW2

---
+++ homeworks/HW2/Folds.hs
@@ -0,0 +1,8 @@
+module Folds where
+
+
+count :: Eq a => a -> [a] -> Integer
+-- Put your implementation here
+
+elem2 :: Eq a => a -> [a] -> Bool
+-- Put your implementation here
---
+++ homeworks/HW2/Zip3unzip3.hs
@@ -0,0 +1,9 @@
+module Zip3unzip3 where
+import Prelude hiding (zip3, unzip3)
+
+zip3 :: [a] -> [b] -> [c] -> [(a,b,c)]
+-- Put your implementation here
+
+unzip3 :: [(a,b,c)] -> ([a], [b], [c])
+-- Put your implementation here
+
