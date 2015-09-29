43b36976578bc9f4d6f5169597a2aa57b5d04965
Tyler Hoyt <tylerhoyt93@gmail.com>
2015-09-29 16:43:45 -0400

    Finished Problem 2 HW2

--- homeworks/HW2/Zip3unzip3.hs
+++ homeworks/HW2/Zip3unzip3.hs
@@ -21,7 +21,16 @@ import Prelude hiding (zip3, unzip3)
 
 zip3 :: [a] -> [b] -> [c] -> [(a,b,c)]
 -- Put your implementation here
+zip3 [] [] [] = []
+zip3 (a:as) [] [] = []
+zip3 [] (b:bs) [] = []
+zip3 [] [] (c:cs) = []
+zip3 (a:as) (b:bs) [] = []
+zip3 (a:as) [] (c:cs) = []
+zip3 [] (b:bs) (c:cs) = []
+zip3 (a:as) (b:bs) (c:cs) = (a,b,c):(zip3 as bs cs)
 
 unzip3 :: [(a,b,c)] -> ([a], [b], [c])
 -- Put your implementation here
-
+-- From prelude source
+unzip3 = foldr (\(a,b,c) ~(as,bs,cs) -> (a:as,b:bs,c:cs)) ([],[],[])
