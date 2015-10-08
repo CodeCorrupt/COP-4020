90402388fc1f5bb1b94918f4916b3040aebc50a1
Pawel Wocjan <wocjan@eecs.ucf.edu>
2015-10-08 19:43:17 -0400

    Added test for matrix mult

--- homeworks/HW3/MatrixTests.hs
+++ homeworks/HW3/MatrixTests.hs
@@ -14,18 +14,29 @@ m10x3 = fillWith (10,3) "u"
 m5x7 = fromRule (5,7) (\(i,j) -> show (i,j))
 m10ipj = fromRule (5,7) (\(i,j) -> show (10*i+j))
 
+delta :: (Int,Int) -> Int
+delta (i,j) 
+  | i == j    = 1::Int
+  | otherwise = 0::Int
+
+rowVal :: (Int,Int) -> Int
+rowVal (i,j) = i 
+
+id3   = fromRule (3,3) delta
+c     = fillWith (3,3) 14
+a     = fromRule (3,3) rowVal
+p     = mult id3 a
+q     = mult (mtranspose a) a
+
+
 tests :: [TestCase String] -- the actual tests
-tests = (map (\(i,j) -> eqTest (initial `at` (i,j)) "==" "initial")
-         (allIndexes (2,3)))
-        ++ (map (\(i,j) -> eqTest (m10x3 `at` (i,j)) "==" "u")
-            (allIndexes (10,3)))
-        ++ (map (\(i,j) -> eqTest (m5x7 `at` (i,j)) "==" (show (i,j)))
-            (allIndexes (5,7)))
-        ++ (map (\(i,j) -> eqTest (m10ipj `at` (i,j)) "==" (show (10*i+j)))
-            (allIndexes (5,7)))
-        ++ (map (\(i,j) -> eqTest ((mtranspose m5x7) `at` (i,j))
-                           "==" (show (j,i)))
-            (allIndexes (7,5)))
-        ++ (map (\(i,j) -> eqTest ((mmap reverse m10ipj) `at` (i,j))
-                           "==" (reverse (show (10*i+j))))
-            (allIndexes (5,7)))
+tests =       (map (\pos -> eqTest (show (a `at` pos))                 "==" (show (p `at` pos)))             (allIndexes (3 ,3)))
+           ++ (map (\pos -> eqTest (show (q `at` pos))                 "==" (show (c `at` pos)))             (allIndexes (3 ,3)))
+
+           ++ (map (\(i,j) -> eqTest (initial `at` (i,j))               "==" "initial")                 (allIndexes (2 ,3)))
+           ++ (map (\(i,j) -> eqTest (m10x3 `at` (i,j))                 "==" "u")                       (allIndexes (10,3)))
+           ++ (map (\(i,j) -> eqTest (m5x7 `at` (i,j))                  "==" (show (i,j)))              (allIndexes (5 ,7)))
+           ++ (map (\(i,j) -> eqTest (m10ipj `at` (i,j))                "==" (show (10*i+j)))           (allIndexes (5 ,7)))
+           ++ (map (\(i,j) -> eqTest ((mtranspose m5x7) `at` (i,j))     "==" (show (j,i)))              (allIndexes (7 ,5)))
+           ++ (map (\(i,j) -> eqTest ((mmap reverse m10ipj) `at` (i,j)) "==" (reverse (show (10*i+j)))) (allIndexes (5 ,7)))
+        
