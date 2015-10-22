1d24bff9b559a6c8de364eb86dcfe474737aca42
Pawel Wocjan <wocjan@eecs.ucf.edu>
2015-10-22 18:55:19 -0400

    Updated CommandLine.hs

--- lectures/haskell-functional/CommandLine.hs
+++ lectures/haskell-functional/CommandLine.hs
@@ -9,7 +9,10 @@ data Command = Eval Expr | Assign Var Expr | Null
 
 commandParse :: Parse Char Command
 
-commandParse = evalParser `alt` assignParser 
+commandParse = evalParser `alt` assignParser `alt` nullParser   
+
+nullParser :: Parse Char Command
+nullParser _ = [(Null,"")]
 
 evalParser :: Parse Char Command
 evalParser = parser `build` Eval
@@ -18,5 +21,5 @@ assignParser :: Parse Char Command
 assignParser = ((spot isVar) >*> (token ':') >*> parser) `build`
                makeAssign
 
---makeAssign (var,(_,expr)) = Assign var expr
-makeAssign t = Assign (fst t) ((snd . snd) t)
+makeAssign (var,(_,expr)) = Assign var expr
+--makeAssign t = Assign (fst t) ((snd . snd) t)
